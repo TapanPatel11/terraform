5bd9037258cbb3509108408fd4d05109740e3541
Tapan Patel <tpnjyptl@gmail.com>
2024-12-06 20:33:26 -0700

    updated s3 name

--- terraform_backend/main.tf
+++ terraform_backend/main.tf
@@ -4,6 +4,6 @@ module "dev-backend" {
   source = "../modules/AWS_Backend"
   app_name = "atlantis"
   environment = "dev"
-  s3_bucket_name = "terraform-state"
+  s3_bucket_name = "tf-state"
   dynamodb_name = "terraform-state"
 }
