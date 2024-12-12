1091f3ed8a08b8e8b0d85240a8a536e70473bf84
Tapan Patel <tpnjyptl@gmail.com>
2024-12-12 15:58:27 -0700

    added basic configs

---
+++ modules/eks/variables.tf
@@ -0,0 +1,20 @@
+var.ami_type
+var.disk_size
+var.instance_types
+var.pblc_desired_size
+var.pblc_max_size
+var.pblc_min_size
+var.node_group_name
+var.eks_cluster_name
+var.nodes_sg_name
+var.cluster_sg_name
+var.endpoint_private_access
+var.endpoint_public_access
+
+var.private_subnet_cidr_blocks
+ var.public_subnet_cidr_blocks
+
+var.vpc_id
+
+var.private_subnet_ids
+var.public_subnet_ids
